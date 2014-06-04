e9d4388f784f5d732ec8c9470d5da62ec2853921
Michele Esposito <mresposito5@gmail.com>
2014-06-04 10:55:39 +0200

    fix tween and anchors

--- app/assets/js/app/index.js
+++ app/assets/js/app/index.js
@@ -7,24 +7,26 @@ require([
 
     ImgLoader.initialize($("body"));
 
+    var duration = 400;
+
 		var controller = new ScrollMagic();
     /* Tweens for the underlining bar */
 	  var tween = TweenMax.to("#life", 0.1, {borderBottom: "2px solid #292c29"});
-	  var scene = new ScrollScene({triggerElement: "#trigger1", duration: 1})
+	  var scene = new ScrollScene({triggerElement: "#trigger1", duration: duration})
 			.setTween(tween)
 			.addTo(controller);
 
-    var secondContact = function(sel1, sel2, trigger) {
+    var secondContact = function(sel1, sel2, trigger, duration) {
       var tween = TweenMax.to(sel1, 0.1, {borderBottom: "2px solid #292c29"});
       var tween2 = TweenMax.to(sel2, 0.1, {borderBottom: "0"});
-      var scene2 = new ScrollScene({triggerElement: trigger, duration: 1})
+      var scene2 = new ScrollScene({triggerElement: trigger, duration: duration})
         .setTween(tween)
         .addTo(controller);
-      var scene2 = new ScrollScene({triggerElement: trigger, duration: 1})
+      var scene2 = new ScrollScene({triggerElement: trigger, duration: duration})
         .setTween(tween2)
         .addTo(controller);
     }
-    secondContact("#work", "#life", "#trigger4");
-    secondContact("#photography", "#work", "#trigger2");
-    secondContact("#contact", "#photography", "#trigger3");
+    secondContact("#work", "#life", "#trigger4", duration);
+    secondContact("#photography", "#work", "#trigger2", duration);
+    secondContact("#contact", "#photography", "#trigger3", 100);
 });
